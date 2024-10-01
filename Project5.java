e3c293759163efb946296899328d819954a37af8
Federico Nicolas Bouvier <136862169+federiconicolasbouvier@users.noreply.github.com>
2024-10-01 18:29:27 -0300

    Add files via upload

---
+++ Project5.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bouvier.project5;
+
+/**
+ *
+ * @author Federico Nicolás Bouvier
+ */
+public class Project5 {
+
+    public static void main(String[] args) {
+        Project5GUI ventana = new Project5GUI();
+        ventana.setVisible(true);
+    }
+}
